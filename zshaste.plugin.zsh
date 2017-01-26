3133240366267f969adc0cf0c1cafdae4cd868a0
Pepe Barbe <dev@antropoide.net>
2017-01-26 11:25:23 -0600

    Haste uses HTTPS, good idea.

--- zshaste.plugin.zsh
+++ zshaste.plugin.zsh
@@ -1,3 +1,3 @@
 haste() { 
-    a=$(cat); curl -X POST -s -d "$a" http://hastebin.com/documents | awk -F '"' '{print "http://hastebin.com/"$4}'; 
+    a=$(cat); curl -X POST -s -d "$a" https://hastebin.com/documents | awk -F '"' '{print "https://hastebin.com/"$4}'; 
 }
